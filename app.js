7a190e6e0e97eaa165023c4c6fe23aceb86a51e9
safil badarudeen <safilbadarudeen@gmail.com>
2022-11-17 17:06:57 +0530

    - added security packages in app

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+//security packages
+const cors = require('cors')
+const helmet = require('helmet')
+const xss = require ('xss-clean')
+const rateLimit = require( ' express-rate-limit')
+
 const express = require('express');
 const app = express();
 
@@ -16,8 +23,17 @@ const jobsRoutes=require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1 )
 
+app.use(rateLimit({
+  windowMs: 15*60*1000,
+  max : 100 
+}))
 app.use(express.json());
+app.use(cors())
+app.use(helmet())
+app.use(xss())
+
 // extra packages
 
 // routes
